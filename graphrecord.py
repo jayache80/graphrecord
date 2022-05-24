2ed8c51a9d9553650cfcc4e7c08b2c1c89809d2b
jim jammer <15721514+jayache80@users.noreply.github.com>
2022-05-23 22:15:15 -0700

    Add graphrecord.py

---
+++ graphrecord.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import csv
+from collections import defaultdict
+from datetime import datetime
+
+graph = defaultdict(int)
+
+def write():
+    global graph
+    now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    file_name = f"output-{now}.csv"
+    print(f"Saving {file_name}")
+    with open(file_name, "w", newline="") as f:
+        writer = csv.writer(f)
+        for interaction, strength in graph.items():
+            src = interaction[0]
+            dst = interaction[1]
+            writer.writerow([src, dst, strength])
+
+def should_quit(s):
+    return s == "quit" or s == "q"
+
+def main():
+    global graph
+    src = input("Enter node (or 'quit' to quit): ")
+    dst = None
+
+    while not should_quit(src):
+        dst = input(f"Who does {src} talk to?: ")
+        if should_quit(dst):
+            break
+        graph[(int(src), int(dst))] += 1
+        src = dst
+
+    write()
+
+    return 0
+
+if __name__ == "__main__":
+    exit(main())
